5a0685fbc341772e5bc582d40a33777d9e678fa9
Shane Lillie <ignignokterr@gmail.com>
2018-07-11 09:43:19 -0700

    Initial import

---
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/OnScreen/OnScreenButton.cs
@@ -0,0 +1,31 @@
+using UnityEngine.EventSystems;
+
+////TODO: pressure support
+
+namespace UnityEngine.Experimental.Input.Plugins.OnScreen
+{
+    /// <summary>
+    /// A button that is visually represented on-screen and triggered by touch or other pointer
+    /// input.
+    /// </summary>
+    public class OnScreenButton : OnScreenControl, IPointerDownHandler, IPointerUpHandler
+    {
+        /// <summary>
+        /// If true, the button's value is driven from the pressure value of touch or pen input.
+        /// </summary>
+        /// <remarks>
+        /// This essentially allows having trigger-like buttons as on-screen controls.
+        /// </remarks>
+        [SerializeField] private bool m_UsePressure;
+
+        public void OnPointerUp(PointerEventData data)
+        {
+            SendValueToControl(0.0f);
+        }
+
+        public void OnPointerDown(PointerEventData data)
+        {
+            SendValueToControl(1.0f);
+        }
+    }
+}
