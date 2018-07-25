dc630b9d25a09e421dd7d99ddc4270ec5cbaed23
Shane Lillie <ignignokterr@gmail.com>
2018-07-25 07:12:43 -0700

    Per-layer camera culling distances

---
+++ Assets/Scripts/Core/Camera/LayerCullingDistance.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace pdxpartyparrot.Core.Camera
+{
+    [RequireComponent(typeof(UnityEngine.Camera))]
+    public class LayerCullingDistance : MonoBehaviour
+    {
+        // TODO: better to do a layer => distance mapping
+        [SerializeField]
+        private float[] _distances = new float[15];
+
+#region Unity Lifecycle
+        private void Start()
+        {
+            float[] actualDistances = new float[15];
+
+            int minLength = System.Math.Min(actualDistances.Length, _distances.Length);
+            for(int i=0; i<minLength; ++i) {
+                actualDistances[i] = _distances[i];
+            }
+            GetComponent<UnityEngine.Camera>().layerCullDistances = actualDistances;
+        }
+#endregion
+    }
+}
