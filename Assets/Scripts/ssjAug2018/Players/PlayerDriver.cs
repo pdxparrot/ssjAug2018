fc4e8fd2da6d1732c080af8e236176d5c8324ee4
Shane Lillie <ignignokterr@gmail.com>
2018-07-27 15:58:41 -0700

    Input clarity

--- Assets/Scripts/ssjAug2018/Players/PlayerDriver.cs
+++ Assets/Scripts/ssjAug2018/Players/PlayerDriver.cs
@@ -48,10 +48,18 @@ namespace pdxpartyparrot.ssjAug2018.Players
 
             if(InputManager.HasInstance) {
                 InputManager.Instance.Controls.game.pause.performed -= OnPause;
-                //InputManager.Instance.Controls.game.move.performed -= OnMove;
-                //InputManager.Instance.Controls.game.look.performed -= OnLook;
+
+                /*InputManager.Instance.Controls.game.move.started -= OnMove;
+                InputManager.Instance.Controls.game.move.performed -= OnMove;
+                InputManager.Instance.Controls.game.move.cancelled -= OnMoveStop;
+
+                InputManager.Instance.Controls.game.look.started -= OnLook;
+                InputManager.Instance.Controls.game.look.performed -= OnLook;
+                InputManager.Instance.Controls.game.look.cancelled -= OnLookStop;*/
+
                 InputManager.Instance.Controls.game.jump.started -= OnJumpStart;
                 InputManager.Instance.Controls.game.jump.performed -= OnJump;
+
                 InputManager.Instance.Controls.game.grab.performed -= OnGrab;
                 InputManager.Instance.Controls.game.drop.performed -= OnDrop;
             }
@@ -66,10 +74,18 @@ namespace pdxpartyparrot.ssjAug2018.Players
 
             if(CanDrive) {
                 InputManager.Instance.Controls.game.pause.performed += OnPause;
-                //InputManager.Instance.Controls.game.move.performed += OnMove;
-                //InputManager.Instance.Controls.game.look.performed += OnLook;
+
+                /*InputManager.Instance.Controls.game.move.started += OnMove;
+                InputManager.Instance.Controls.game.move.performed += OnMove;
+                InputManager.Instance.Controls.game.move.cancelled += OnMoveStop;
+
+                InputManager.Instance.Controls.game.look.started += OnLook;
+                InputManager.Instance.Controls.game.look.performed += OnLook;
+                InputManager.Instance.Controls.game.look.cancelled += OnLookStop;*/
+
                 InputManager.Instance.Controls.game.jump.started += OnJumpStart;
                 InputManager.Instance.Controls.game.jump.performed += OnJump;
+
                 InputManager.Instance.Controls.game.grab.performed += OnGrab;
                 InputManager.Instance.Controls.game.drop.performed += OnDrop;
             }
@@ -85,7 +101,7 @@ namespace pdxpartyparrot.ssjAug2018.Players
             PartyParrotManager.Instance.TogglePause();
         }
 
-        /*private void OnMove(InputAction.CallbackContext ctx)
+        private void OnMove(InputAction.CallbackContext ctx)
         {
             if(!IsOurGamepad(ctx) || !CanDrive) {
                 return;
@@ -93,6 +109,15 @@ namespace pdxpartyparrot.ssjAug2018.Players
 
             Vector2 axes = ApplyDeadZone(ctx.ReadValue<Vector2>());
             LastMoveAxes = new Vector3(axes.x, axes.y, 0.0f);
+        }
+
+        private void OnMoveStop(InputAction.CallbackContext ctx)
+        {
+            if(!IsOurGamepad(ctx) || !CanDrive) {
+                return;
+            }
+
+            LastMoveAxes = new Vector3(0.0f, 0.0f, 0.0f);
         }
 
         private void OnLook(InputAction.CallbackContext ctx)
@@ -105,7 +130,16 @@ namespace pdxpartyparrot.ssjAug2018.Players
             axes.y *= _invertLookY ? -1 : 1;
 
             Player.FollowTarget.LastLookAxes = new Vector3(axes.x, axes.y, 0.0f);
-        }*/
+        }
+
+        private void OnLookStop(InputAction.CallbackContext ctx)
+        {
+            if(!IsOurGamepad(ctx) || !CanDrive) {
+                return;
+            }
+
+            Player.FollowTarget.LastLookAxes = new Vector3(0.0f, 0.0f, 0.0f);
+        }
 
         private void OnJumpStart(InputAction.CallbackContext ctx)
         {
