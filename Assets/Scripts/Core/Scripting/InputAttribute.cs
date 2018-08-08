2c9ce43e0c528f2ac5f66567af230d727f99dcc5
Shane Lillie <ignignokterr@gmail.com>
2018-08-07 18:04:07 -0700

    Script attributes

---
+++ Assets/Scripts/Core/Scripting/InputAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace pdxpartyparrot.Core.Scripting
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class InputAttribute : Attribute
+    {
+    }
+}
