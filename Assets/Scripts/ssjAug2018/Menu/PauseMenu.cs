5c243b46429ffb466848874ffe0e3133cfac1349
Shane Lillie <ignignokterr@gmail.com>
2018-08-07 23:48:50 -0700

    Settings menu skeleton

--- Assets/Scripts/ssjAug2018/Menu/PauseMenu.cs
+++ Assets/Scripts/ssjAug2018/Menu/PauseMenu.cs
@@ -6,10 +6,20 @@ namespace pdxpartyparrot.ssjAug2018.Menu
 {
     public sealed class PauseMenu : MenuPanel
     {
+        [SerializeField]
+        private SettingsMenu _settingsMenu;
+
+#region Unity Lifecycle
+        private void Awake()
+        {
+            _settingsMenu.gameObject.SetActive(false);
+        }
+#endregion
+
 #region Event Handlers
         public void OnSettings()
         {
-            Debug.Log("TODO: settings");
+            Owner.PushPanel(_settingsMenu);
         }
 
         public void OnExit()
---
+++ Assets/Scripts/ssjAug2018/Menu/SettingsMenu.cs
@@ -0,0 +1,14 @@
+﻿using pdxpartyparrot.Game.Menu;
+
+namespace pdxpartyparrot.ssjAug2018.Menu
+{
+    public sealed class SettingsMenu : MenuPanel
+    {
+#region Event Handlers
+        public void OnBack()
+        {
+            Owner.PopPanel();
+        }
+#endregion
+    }
+}
